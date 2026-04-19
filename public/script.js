5c358817a4a3b9b030abf31c3b106fb60c0953b6
Iale Leles <ialeleles@proton.me>
2026-04-19 14:23:52 -0300

    feat: adiciona prompt de despesas e while para validação do número de despesas.

--- public/script.js
+++ public/script.js
@@ -3,3 +3,16 @@ console.log("Simulador simples de orçamento pessoal")
 let nome = prompt("Digite seu nome: ")
 
 let renda = Number(prompt("Digite sua renda: "))
+
+while (isNaN(renda))
+{renda = Number(prompt("Digite sua renda novamente: "))}
+
+let despesas = Number(prompt("Digite a quantidade de despesas: "))
+
+while (isNaN(despesas))
+{despesas = Number(prompt("Digite a quantidade de despesas novamente: "))}
+if (despesas < 1) {
+    despesas = 1;
+} if (despesas > 5) {
+    despesas = 5;
+}
